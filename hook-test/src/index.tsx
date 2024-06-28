44e49c3dc2ca02063d189eae380b719b0c02499c
冯英祥 <694838286@qq.com>
2024-06-28 18:02:28 +0800

    feat:useEffect

--- hook-test/src/index.tsx
+++ hook-test/src/index.tsx
@@ -1,19 +1,27 @@
+/*
+ * @Author: 冯英祥 694838286@qq.com
+ * @Date: 2024-06-28 17:08:36
+ * @LastEditors: 冯英祥 694838286@qq.com
+ * @LastEditTime: 2024-06-28 17:51:46
+ * @FilePath: \学习\NG\react-course-code-learn\hook-test\src\index.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App2';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
