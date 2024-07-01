1ca325ff199bae1379608178204f7176f1c0eaee
niuge <694838286@qq.com>
2024-07-01 23:33:16 +0800

    feat:日历

---
+++ calendar-test/src/App.tsx
@@ -0,0 +1,66 @@
+/*
+ * @Author: niuge 694838286@qq.com
+ * @Date: 2024-07-01 22:43:32
+ * @LastEditors: niuge 694838286@qq.com
+ * @LastEditTime: 2024-07-01 22:44:55
+ * @FilePath: \react-course-code-learn\calendar-test\src\App.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import React from 'react';
+import './index.css';
+
+function Calendar() {
+  return (
+    <div className="calendar">
+      <div className="header">
+        <button>&lt;</button>
+        <div>2023 年 7 月</div>
+        <button>&gt;</button>
+      </div>
+      <div className="days">
+        <div className="day">日</div>
+        <div className="day">一</div>
+        <div className="day">二</div>
+        <div className="day">三</div>
+        <div className="day">四</div>
+        <div className="day">五</div>
+        <div className="day">六</div>
+        <div className="empty"></div>
+        <div className="empty"></div>
+        <div className="day">1</div>
+        <div className="day">2</div>
+        <div className="day">3</div>
+        <div className="day">4</div>
+        <div className="day">5</div>
+        <div className="day">6</div>
+        <div className="day">7</div>
+        <div className="day">8</div>
+        <div className="day">9</div>
+        <div className="day">10</div>
+        <div className="day">11</div>
+        <div className="day">12</div>
+        <div className="day">13</div>
+        <div className="day">14</div>
+        <div className="day">15</div>
+        <div className="day">16</div>
+        <div className="day">17</div>
+        <div className="day">18</div>
+        <div className="day">19</div>
+        <div className="day">20</div>
+        <div className="day">21</div>
+        <div className="day">22</div>
+        <div className="day">23</div>
+        <div className="day">24</div>
+        <div className="day">25</div>
+        <div className="day">26</div>
+        <div className="day">27</div>
+        <div className="day">28</div>
+        <div className="day">29</div>
+        <div className="day">30</div>
+        <div className="day">31</div>
+      </div>
+    </div>
+  );
+}
+
+export default Calendar;
